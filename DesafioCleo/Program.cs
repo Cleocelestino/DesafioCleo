08c7430df4f21294d55b9438fe34713efadca68c
Cleo Celestino <cleocelestinoo@gmail.com>
2020-02-28 19:04:30 -0300

    Update Program.cs

--- DesafioCleo/Program.cs
+++ DesafioCleo/Program.cs
@@ -6,11 +6,11 @@ namespace DesafioCleo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva o nome completo);
-            string nome = Console.ReadLine()
-            Console.WhriteLine("CONTRATO DE ADESÃO/n" + "Obrigado Sr(a)" +usuario+ " por contratar o nosso serviço.");
-            Console.WriteLine("Assine abaixo/n");
-            Console.WriteLine("nome completo".ToUpper());
+            Console.WriteLine("Escreva o nome completo");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Contrato de Adesão\n" +
+                "Obrigado Sr(a) " + nome + " por contratar o nosso serviço.\n" +
+                "Assine abaixo\n\n\n" + nome.ToUpper());
 
 
         }
