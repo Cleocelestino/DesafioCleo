bfccda7f9f7d250125c634ff86aae8031453e037
Cleo Celestino <cleocelestinoo@gmail.com>
2020-02-28 17:54:50 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioCleo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesafioCleo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o nome completo);
+            string nome = Console.ReadLine()
+            Console.WhriteLine("CONTRATO DE ADESÃO/n" + "Obrigado Sr(a)" +usuario+ " por contratar o nosso serviço.");
+            Console.WriteLine("Assine abaixo/n");
+            Console.WriteLine("nome completo".ToUpper());
+
+
+        }
+    }
+}
